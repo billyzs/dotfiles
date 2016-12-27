ed30bb0bb061e68b7c9f9490df187a9a5605824a
Shao Zhou <billyzs@users.noreply.github.com>
2016-12-27 23:27:48 +0800

    Update pythonrc.py

--- dev/.virtualenvs/cv/pythonrc.py
+++ dev/.virtualenvs/cv/pythonrc.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Best goddamn .pythonrc file in the whole world.
-This file is executed when the Python interactive shell is started if
-$PYTHONSTARTUP is in your environment and points to this file. It's just
-regular Python commands, so do what you will. Your ~/.inputrc file can greatly
-complement this file.
-"""
+
 # Imports we need
 import sys
 import os
