5a33003b9d9f383129222b4969da70e43463bf96
Billy Zhou <billyzs.728@gmail.com>
2019-09-24 10:46:54 -0700

    add xrandr scripts for screen layout

---
+++ .config/zsh_local/bin/lp.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-3.1 --off --output DP-3.2 --off --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --primary --mode 1920x1080 --panning 0x0 --pos 0x0 --rotate normal --output DP-5 --off --output DP-6 --off
---
+++ .config/zsh_local/bin/pas.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --primary --mode 1920x1080 --pos 0x1200 --rotate normal --output DP-5 --off --output DP-6 --off --output DP-3.1 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP-3.2 --mode 1920x1200 --pos 0x0 --rotate normal
